acf4af03d146b4932aea68bdc1dd4c1c8636ceed
Tom <info@toxictoast.de>
2018-03-24 03:07:28 +0100

    updated ranked view

--- src/app/ranked/components/ranking-list/ranking-list.component.ts
+++ src/app/ranked/components/ranking-list/ranking-list.component.ts
@@ -14,4 +14,43 @@ export class RankingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  getPlayerTier(tier: string) {
+    return `/assets/ranks/${tier}.png`;
+  }
+
+  getRankingWithDiff(ranking: number, trends: any[]): string {
+    let returnString = '';
+    //
+    if (trends.length > 0) {
+      const lastRank = trends[0];
+      const { player_rank } = lastRank;
+      const difference = ranking - player_rank;
+      const icon = this.getDiffIcon(difference);
+      const symbol = this.getDiffSymbol(difference);
+      if (difference !== 0) {
+        returnString += ` ( ${symbol} ${difference} ${icon} )`;
+      }
+    }
+    //
+    return returnString;
+  }
+
+  private getDiffIcon(difference: number): string {
+    if (difference < 0) {
+      return `<i class='fa fa-caret-down'></i>`;
+    } else if (difference > 0) {
+      return `<i class='fa fa-caret-up'></i>`;
+    } else {
+      return '';
+    }
+  }
+
+  private getDiffSymbol(difference: number): string {
+    if (difference > 0) {
+      return `+`;
+    } else {
+      return '';
+    }
+  }
+
 }
