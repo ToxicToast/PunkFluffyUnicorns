40244fe35ac1856b5de8b180b3f11892d8a158a4
Tom <info@toxictoast.de>
2018-04-10 01:17:23 +0200

    added tierlist module

---
+++ src/app/tierlist/tierlist.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TierlistRoutingModule } from './tierlist-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TierlistRoutingModule
+  ],
+  declarations: []
+})
+export class TierlistModule { }
