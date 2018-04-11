b5a3b6a7ec4e6aec999b380dce2e058c3867ef6c
Tom <info@toxictoast.de>
2018-04-11 18:55:22 +0200

    fixed add vod

--- src/app/vod/effects/vods.effects.ts
+++ src/app/vod/effects/vods.effects.ts
@@ -9,6 +9,8 @@ import { Action } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
 
 import { VodService } from '@vod/services/vod.service';
+import { ToastService } from '@core/services/toast.service';
+
 
 import {
   LoadVideosSuccess,
@@ -25,27 +27,38 @@ export class VodsEffects {
 
   constructor(
     private actions: Actions,
-    private service: VodService
+    private service: VodService,
+    private toastr: ToastService
   ) { }
 
   @Effect()
   loadVideos$ = this.actions.ofType(VodActionTypes.LOAD_VOD)
     .switchMap(() => this.service.getVideos()
       .map(data => new LoadVideosSuccess(this.getVodQuery(data)))
-      .catch(err => of(new LoadVideosFailure())));
+      .catch(err => {
+        this.toastr.showError('Failed loading VODs');
+        return of(new LoadVideosFailure());
+      }));
 
     @Effect()
     loadSingle$ = this.actions.ofType(VodActionTypes.LOAD_SINLGEVOD)
       .switchMap(payload => this.service.getSingle(payload)
-        .map(data => new LoadSingleVideoSuccess(this.getVodQuery(data)))
-      .catch(err => of(new LoadSingleVideoFailure())));
+      .map(data => new LoadSingleVideoSuccess(this.getVodQuery(data)))
+      .catch(err => {
+        this.toastr.showError('Failed loading VOD');
+        return of(new LoadSingleVideoFailure());
+      }));
 
       @Effect()
       addVideo$ = this.actions.ofType(VodActionTypes.ADD_VOD)
         .switchMap(payload => this.service.addVideo(payload)
-          .map(data => new AddVideoSuccess())
-          .catch(err => of(new AddVideoFailure(err))));
-
+          .map(data => {
+            this.toastr.showSuccess('Successfully adding VOD');
+            return of(new AddVideoSuccess());
+          }).catch(err => {
+              this.toastr.showError('Failed adding VOD');
+              return of(new LoadSingleVideoFailure());
+            }));
 
   private getVodQuery(payload) {
     const { vodQuery } = payload.data;
