4294f08467bc30e9df7b2cfc08d64f2f40392312
Tom <info@toxictoast.de>
2018-04-16 01:40:52 +0200

    added guides

--- src/app/guides/components/guides-list/guides-list.component.ts
+++ src/app/guides/components/guides-list/guides-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-guides-list',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuidesListComponent implements OnInit {
 
+  @Input() state: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  trackByTitle(index: number, guide: any) {
+    return guide.title;
   }
 
 }
