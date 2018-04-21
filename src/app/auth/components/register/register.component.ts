adff27af6fed133c886334aba1b1aa62aec51d05
Tom <info@toxictoast.de>
2018-04-21 14:11:32 +0200

    updated register

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { OverwatchRoles } from '@core/helpers/Roles';
 
 @Component({
   selector: 'app-register',
@@ -7,11 +8,16 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  roles = [];
   @Output() goToLogin = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
+    this.roles.push(OverwatchRoles.ROLE_DPS);
+    this.roles.push(OverwatchRoles.ROLE_FLEX);
+    this.roles.push(OverwatchRoles.ROLE_SUPPORT);
+    this.roles.push(OverwatchRoles.ROLE_TANK);
   }
 
 }
