f4a0433dc96914924c4815c8c33a10983c6b9ca1
Kortyka Thomas <tkortyka@alegri.eu>
2018-04-24 13:13:50 +0200

    fixed matches module

--- src/app/matches/matches.module.ts
+++ src/app/matches/matches.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { CoreModule } from '@core/core.module';
+
 import { MatchesRoutingModule } from '@matches/matches-routing.module';
 import { MatchIndexContainerComponent } from '@matches/containers/match-index-container/match-index-container.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatchesRoutingModule
+    MatchesRoutingModule,
+    // StoreModule.forFeature('matches', reducers),
+    EffectsModule.forFeature([]),
+    CoreModule,
   ],
   declarations: [
+    MatchIndexContainerComponent
+  ],
+  providers: [
+
+  ],
+  exports: [
     MatchIndexContainerComponent
   ]
 })
