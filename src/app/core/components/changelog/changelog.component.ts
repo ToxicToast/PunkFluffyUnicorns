1bf343eaacd028f6a95415b781a0322ec7961743
ToxicToast <info@toxictoast.de>
2018-05-07 22:05:41 +0200

    changelog animation

--- src/app/core/components/changelog/changelog.component.ts
+++ src/app/core/components/changelog/changelog.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from '@core/helpers/Animation';
 
 @Component({
   selector: 'app-changelog',
   templateUrl: './changelog.component.html',
-  styleUrls: ['./changelog.component.scss']
+  styleUrls: ['./changelog.component.scss'],
+  animations: [fadeAnimation]
 })
 export class ChangelogComponent implements OnInit {
 
